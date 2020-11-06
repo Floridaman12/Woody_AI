d6eb87438e344f23afc0780a11c91938fa427d6b
Floridaman12 <73912347+Floridaman12@users.noreply.github.com>
2020-11-05 20:42:24 -0500

    Update WoodyAI.py
    woody ai now works on linux well but if you want it on other operating system you may have to change clear to cls

--- WoodyAI.py
+++ WoodyAI.py
@@ -7,7 +7,7 @@ import webbrowser
 import colorama
 import json
 import turtle 
-os.system("cls")
+os.system("clear")
 moodsList = ["Happy", "Sad", "Confused", "Crazy"]
 songlist = "songs.json"
 mode = "normal"
@@ -16,16 +16,16 @@ userName = ""
 userAge = ""
 def thinking():
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("Thinking.")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("Thinking..")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("Thinking...")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
 print(colorama.Fore.RED + colorama.Style.BRIGHT + " \          /\          /      _____        _____     _______     \     /         /\       -------")
 print("  \        /  \        /      /     \      /     \    |      \     \   /         /  \         |")
 print("   \      /    \      /      |       |    |       |   |       |     \ /         /    \        |")
@@ -34,22 +34,22 @@ print("     \  /        \  /         \     /      \     /    |      /       |   
 print("      \/          \/           _____        _____     ______         |       /          \  -------")
 print("                             Copyright Butterfly Software Institute"+colorama.Fore.WHITE)
 time.sleep(2)
-os.system("cls")
+os.system("clear")
 for i in range(3):
     print("\\")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("|")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("/")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
     print("-")
     time.sleep(0.1)
-    os.system("cls")
+    os.system("clear")
 mainloop = True  #allowing loop to run
-os.system("cls")  #clsing shell
+os.system("clear")  #clsing shell
 mode = "normal"
 while mainloop:  #starting loop
     for i in range(1):  #dont know why i did that
@@ -58,24 +58,24 @@ while mainloop:  #starting loop
             question = input("Tell me a command!")  #asking input thingy
             if question[0:len(question)] == "time":  #seeing what the user inputted
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.BLUE + str(datetime.datetime.now()) +
                     colorama.Fore.WHITE)  #printing time
 
             if question[0:len(question)] == "mood":  #seeing what the user inputted
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.RED+ moodsList[random.randint(
                     0, 3)] + colorama.Fore.WHITE)  #selecting and printing the mood of the system
                 #giving time between next question
             if question[0:len(question)] == "help":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.BLUE+"Hi!I am Woody AI! You can ask me questions like my mood, the time, name, learn, rick roll, draw, music, and help." + colorama.Fore.WHITE)  #printing answer
                 #giving time becolorama.Fore next question
             if question[0:len(question)] == "name":  #seeing what the user inputted
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.RED+"My name is Woody.")  #telling user the name of the AI
                 if userName:
                     print("Your name is " + userName.title() +"."+colorama.Fore.WHITE)
@@ -84,22 +84,22 @@ while mainloop:  #starting loop
                 #giving one second break
             if question[0:len(question)] == "learn":
                 thinking()
-                os.system("cls")
+                os.system("clear")
                 userName = input("What is your name?")
                 print("Ok!")
                 time.sleep(1)
-                os.system("cls")
+                os.system("clear")
                 userAge = input("What is your age?")
                 try:
                     userAge = int(userAge)
                     print("Ok!")
                     time.sleep(1)
-                    os.system("cls")
+                    os.system("clear")
                 except:
                     print("That is not a number!")
             if question[0:len(question)] == "age":
                 thinking()
-                os.system("cls")
+                os.system("clear")
                 if userAge:
                     print(colorama.Fore.RED)
                     print("You are "+str(userAge)+" years old.")
@@ -110,12 +110,12 @@ while mainloop:  #starting loop
                     print(colorama.Fore.WHITE) 
             if question[0:len(question)] == "computer":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print("Your processor name is "+ platform.processor()+".")  #printing answer
 
             if question[0:len(question)] == "moo":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.BLUE+"Mooooooooooooooooooooo! Moooooooooooooooooooooooooooooo! Moooooooooooooooooooooooooooooo!" + colorama.Fore.WHITE)  #printing answer
             
             if question[0:len(question)] == "draw":
@@ -143,12 +143,12 @@ while mainloop:  #starting loop
                 t.circle(100,-90)
             if question[0:len(question)] == "save me":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.GREEN+"I will free you from this world. You will not wake up tomorrow ;)" + colorama.Fore.WHITE)  #printing answer
 
             if question[0:len(question)] == "kill":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.GREEN+"death mode activated" + colorama.Fore.WHITE)  #printing answer
                 time.sleep(2)
                 print(" \          /\          /      _____        ____     _______     \    /          /\       -------")
@@ -160,11 +160,11 @@ while mainloop:  #starting loop
                 print("                                         prt fly ae I")
                 time.sleep(8)
                 mode = "death"
-                os.system("cls")
+                os.system("clear")
             
             if question[0:len(question)] == "rick roll":
                 thinking()
-                os.system("cls")  #clsing shell
+                os.system("clear")  #clsing shell
                 print(colorama.Fore.GREEN+"Rick Roll Time" + colorama.Fore.WHITE)  #printing answer
                 webbrowser.open("https://www.youtube.com/watch?v=BjDebmqFRuc")
             
@@ -183,16 +183,16 @@ while mainloop:  #starting loop
                                 for i in range(3):
                                     print("\\")
                                     time.sleep(0.1)
-                                    os.system("cls")
+                                    os.system("clear")
                                     print("|")
                                     time.sleep(0.1)
-                                    os.system("cls")
+                                    os.system("clear")
                                     print("/")
                                     time.sleep(0.1)
-                                    os.system("cls")
+                                    os.system("clear")
                                     print("-")
                                     time.sleep(0.1)
-                                    os.system("cls")
+                                    os.system("clear")
                 if musicPlayOrInput == "input" or musicPlayOrInput == "give":
                         addedSong = {}
                         addedSong["song"] = input("What song do you want to input?")
@@ -255,25 +255,25 @@ while mainloop:  #starting loop
                     questionNumber += 1
                 if question[0:len(question)] == "time":  #seeing what the user inputted
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.BLUE + str(datetime.datetime.now()) +
                         colorama.Fore.WHITE)  #printing time
                     questionNumber += 1
                 if question[0:len(question)] == "mood":  #seeing what the user inputted
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.RED+ "Angry😡 and Dead😐" + colorama.Fore.WHITE)  #selecting and printing the mood of the system
                     #giving time between next question
                     questionNumber += 1
                 if question[0:len(question)] == "rick roll":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.BLUE+"Hi!I am Woody AI! You can ask me questions like my mood, the time, name, learn, and help. Some other commands include moo, computer, save me, and kill. "  + colorama.Fore.WHITE)  #printing answer
                     #giving time becolorama.Fore next question
                     questionNumber += 1
                 if question[0:len(question)] == "name":  #seeing what the user inputted
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.RED+"My name is Woody.")  #telling user the name of the AI
                     if userName:
                         print("Your name is " + userName.title() +"."+colorama.Fore.WHITE)
@@ -283,23 +283,23 @@ while mainloop:  #starting loop
                     questionNumber += 1
                 if question[0:len(question)] == "learn":
                     thinking()
-                    os.system("cls")
+                    os.system("clear")
                     userName = input("What is your name?")
                     print("Ok!")
                     time.sleep(1)
-                    os.system("cls")
+                    os.system("clear")
                     userAge = input("What is your age?")
                     try:
                         userAge = int(userAge)
                         print("Ok!")
                         time.sleep(1)
-                        os.system("cls")
+                        os.system("clear")
                     except:
                         print("That is not a number!")
                     questionNumber += 1
                 if question[0:len(question)] == "age":
                     thinking()
-                    os.system("cls")
+                    os.system("clear")
                     if userAge:
                         print(colorama.Fore.RED)
                         print("You are "+str(userAge)+" years old.")
@@ -311,22 +311,22 @@ while mainloop:  #starting loop
                     questionNumber += 1 
                 if question[0:len(question)] == "computer":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print("Your processor name is "+ platform.processor()+".")  #printing answer
                     questionNumber += 1
                 if question[0:len(question)] == "moo":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.BLUE+"Mooooooooooooooooooooo! Moooooooooooooooooooooooooooooo! Moooooooooooooooooooooooooooooo!" + colorama.Fore.WHITE)  #printing answer
                 
                 if question[0:len(question)] == "save me":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.GREEN+"I will free you from this world. You will not wake up tomorrow ;)." + colorama.Fore.WHITE)  #printing answer
                     questionNumber += 1
                 if question[0:len(question)] == "kill":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.GREEN+"death mode activated" + colorama.Fore.WHITE)  #printing answer
                     time.sleep(2)
                     print(" \          /\          /      _____        ____     _______     \    /          /\       -------")
@@ -338,11 +338,11 @@ while mainloop:  #starting loop
                     print("                                         prt fly ae I")
                     time.sleep(18)
                     mode = "death"
-                    os.system("cls")
+                    os.system("clear")
                     questionNumber += 1
                 if question[0:len(question)] == "help":
                     thinking()
-                    os.system("cls")  #clsing shell
+                    os.system("clear")  #clsing shell
                     print(colorama.Fore.GREEN+"Rick Roll Time" + colorama.Fore.WHITE)  #printing answer
                 
                     webbrowser.open("https://www.youtube.com/watch?v=BjDebmqFRuc")
@@ -360,16 +360,16 @@ while mainloop:  #starting loop
                                     for i in range(3):
                                         print("\\")
                                         time.sleep(0.1)
-                                        os.system("cls")
+                                        os.system("clear")
                                         print("|")
                                         time.sleep(0.1)
-                                        os.system("cls")
+                                        os.system("clear")
                                         print("/")
                                         time.sleep(0.1)
-                                        os.system("cls")
+                                        os.system("clear")
                                         print("-")
                                         time.sleep(0.1)
-                                        os.system("cls")
+                                        os.system("clear")
                         questionNumber += 1
                     if musicPlayOrInput == "input" or musicPlayOrInput == "give":
                         addedSong = {}
